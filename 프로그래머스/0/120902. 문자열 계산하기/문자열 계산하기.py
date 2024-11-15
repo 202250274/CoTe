7fe8d39e776a798bec0a8382388c5cbc4473e6b1
jinyeong.Park <91312489+202250274@users.noreply.github.com>
2024-11-15 15:06:26 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,3 +1,11 @@
 def solution(my_string):
-    answer = eval(my_string)
+    split_str = my_string.split(' ')
+    
+    answer=int(split_str[0])
+    for i in range(1,len(split_str),2):
+        if split_str[i] == '+':
+            answer+=int(split_str[i+1])
+        else:
+            answer-=int(split_str[i+1])
+
     return answer
