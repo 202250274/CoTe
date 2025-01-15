b29a460744369252eab53e7391233572f4d78c8b
jinyeong.Park <91312489+202250274@users.noreply.github.com>
2025-01-15 22:46:21 +0900

    [Bronze II] Title: 분해합, Time: 908 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,7 @@
 n = int(input())
 num = 0
 for i in range(int(n/1000),n+1):
-    n_str = list(str(i))
-    if i+int(sum(map(int,n_str))) == n:
+    if i+sum(map(int,str(i))) == n:
         num+=i
         break
 print(num)
