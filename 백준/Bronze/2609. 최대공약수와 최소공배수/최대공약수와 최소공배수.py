eb28edfac9fda602546a725454a43870d1af9069
jinyeong.Park <91312489+202250274@users.noreply.github.com>
2025-01-19 21:22:09 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,22 @@
+import math
+a, b = map(int, input().split())
+print(math.gcd(a,b))
+print(math.lcm(a,b))
+# n=1
+# m=1
+# while True:
+#     if n*a == b*m:
+#         print(n*a)
+#         break
+#     elif n*a < b*m:
+#         n+=1
+#     else:
+#         while True:
+#             m+=1
+#             if n*a >= m*b:
+#                 break
+        
+        
+    
+    
+    
